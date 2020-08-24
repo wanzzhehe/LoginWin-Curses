408127e32b42e434ed98a8bd7069600de1dbc0c1
wanzzhehe <wanzzmicro@outlook.com>
2020-08-24 19:15:47 +0800

    2020-08-24

--- main.cpp
+++ main.cpp
@@ -11,60 +11,60 @@ int main() {
     curs_set(0);
     init_pair(1, COLOR_BLUE, COLOR_GREEN);
     init_pair(2, COLOR_WHITE, COLOR_MAGENTA);
-    list[0] = new InputWin();
-    list[1] = new InputWin();
-    list[2] = new Buttom();
-    list[3] = new Buttom();
-    mvwprintw(stdscr, 5, 0, "Username: ");
-    mvwprintw(stdscr, 7, 0, "Password: ");
-    wrefresh(stdscr);
-    ((InputWin *)list[0])->Init(5, 10, 1, 40, 1, 2, InputWin::NAME);
-    ((InputWin *)list[1])->Init(7, 10, 1, 40, 1, 2, InputWin::PASS);
-    ((Buttom *)list[2])->Init(9, 16, 1, 8, 1, "<  OK  >");
-    ((Buttom *)list[3])->Init(9, 36, 1, 8, 1, "<Cancel>");
-    for(int i = 0; i < 4; ++i) list[i]->Refresh();
-    int i;
-    for(i = 0; ; ++i, i %= 4) {
-        chtype ch = list[i]->Select();
-        switch(ch) {
-            case 0x0A: {
-                if(i == 2 || i == 3) goto BREAK;
-                break;
-            }
-            case 0x09: {
-                break;
-            }
-            case KEY_UP: {
-                if(i == 0) i = 1;
-                else if(i == 1) i = 3;
-                else if(i == 2 || i == 3) i = 0;
-                break;
-            }
-            case KEY_DOWN: {
-                if(i == 2) i = 3;
-                break;
-            }
-            case KEY_LEFT: {
-                if(i == 3) i = 1;
-                break;
-            }
-            case KEY_RIGHT: {
-                if(i == 3) i = 1;
-                break;
-            }
-            default: {
-                break;
-            }
-        }
-    }
-BREAK:
-    curs_set(0);
-    endwin();
-    if(i == 2) {
-        printf("Your username is \"%s\"\n", (((InputWin *)list[0])->GetText()).c_str());
-        printf("Your password is \"%s\"\n", (((InputWin *)list[1])->GetText()).c_str());
-    } else if(i == 3) {
-        printf("You cancel your login.\n");
-    }
+//    list[0] = new InputWin();
+//    list[1] = new InputWin();
+//    list[2] = new Buttom();
+//    list[3] = new Buttom();
+//    mvwprintw(stdscr, 5, 0, "Username: ");
+//    mvwprintw(stdscr, 7, 0, "Password: ");
+//    wrefresh(stdscr);
+//    ((InputWin *)list[0])->Init(5, 10, 1, 40, 1, 2, InputWin::NAME);
+//    ((InputWin *)list[1])->Init(7, 10, 1, 40, 1, 2, InputWin::PASS);
+//    ((Buttom *)list[2])->Init(9, 16, 1, 8, 1, "<  OK  >");
+//    ((Buttom *)list[3])->Init(9, 36, 1, 8, 1, "<Cancel>");
+//    for(int i = 0; i < 4; ++i) list[i]->Refresh();
+//    int i;
+//    for(i = 0; ; ++i, i %= 4) {
+//        chtype ch = list[i]->Select();
+//        switch(ch) {
+//            case 0x0A: {
+//                if(i == 2 || i == 3) goto BREAK;
+//                break;
+//            }
+//            case 0x09: {
+//                break;
+//            }
+//            case KEY_UP: {
+//                if(i == 0) i = 1;
+//                else if(i == 1) i = 3;
+//                else if(i == 2 || i == 3) i = 0;
+//                break;
+//            }
+//            case KEY_DOWN: {
+//                if(i == 2) i = 3;
+//                break;
+//            }
+//            case KEY_LEFT: {
+//                if(i == 3) i = 1;
+//                break;
+//            }
+//            case KEY_RIGHT: {
+//                if(i == 3) i = 1;
+//                break;
+//            }
+//            default: {
+//                break;
+//            }
+//        }
+//    }
+//BREAK:
+//    curs_set(0);
+//    endwin();
+//    if(i == 2) {
+//        printf("Your username is \"%s\"\n", (((InputWin *)list[0])->GetText()).c_str());
+//        printf("Your password is \"%s\"\n", (((InputWin *)list[1])->GetText()).c_str());
+//    } else if(i == 3) {
+//        printf("You cancel your login.\n");
+//    }
     return 0;
 }
--- src/LoginWin.cpp
+++ src/LoginWin.cpp
@@ -1,9 +1,7 @@
 #include "LoginWin.h"
 
-LoginWin::LoginWin() { ;}
-
-void LoginWin::Init(int y, int x, int H, int W, short p, STATUS s, string tm) {
-    BaseWin::Init(y, x, H, W, p);
+void LoginWin::Init(int y, int x, int H, int W, short p0, short p1, short p2, short p3, STATUS s, string tm) {
+    BaseWin::Init(y, x, H, W, p0);
     stat = s, topmesg = tm;
     if(topmesg.length() == 0) {
         switch(stat) {
@@ -15,11 +13,31 @@ void LoginWin::Init(int y, int x, int H, int W, short p, STATUS s, string tm) {
                 break;
         }
     }
+    B_Okey.Init(starty + height - 2, startx + width / 4 - 4, 1, 8, p1, "< Okey >");
+    B_Cancel.Init(starty + height - 2, startx + 3 * width / 4 - 4, 1, 8, p1, "<Cancel>");
+    //IW.push_back(InputWin());
+    //IW.push_back(InputWin());
+    //IW[0].Init(starty + 3, startx + 11, 1, width - 12, p2, p3, InputWin::NAME);
+    //IW[1].Init(starty + 5, startx + 11, 1, width - 12, p2, p3, InputWin::PASS);
+    //if(REGISTER == stat) {
+    //    IW.push_back(InputWin());
+    //    IW[2].Init(starty + 7, )
     switch(stat) {
-        LOGIN:
-            
+        case LOGIN: {
+            IW.push_back(InputWin());
+            IW.push_back(InputWin());
+            IW[0].Init(starty + 3, startx + 11, 1, width - 12, p2, p3, InputWin::NAME);
+            IW[1].Init(starty + 5, startx + 11, 1, width - 12, p2, p3, InputWin::PASS);
             break;
-        REGISTER:
+        }
+        case REGISTER: {
+            IW.push_back(InputWin());
+            IW.push_back(InputWin());
+            IW.push_back(InputWin());
+            IW[0].Init(starty + 3, startx + 19, 1, width - 20, p2, p3, InputWin::NAME);
+            IW[1].Init(starty + 5, startx + 19, 1, width - 20, p2, p3, InputWin::PASS);
+            IW[2].Init(starty + 7, startx + 19, 1, width - 20, p2, p3, InputWin::PASS);
             break;
+        }
     }
 }
