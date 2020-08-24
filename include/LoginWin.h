c9a44378c868ed327280ad3d1b01f3ee2315a4ea
wanzzhehe <wanzzmicro@outlook.com>
2020-08-24 18:40:11 +0800

    2020-8-24

--- include/LoginWin.h
+++ include/LoginWin.h
@@ -1,4 +1,22 @@
 #ifndef __LOGINWIN_H__
 #define __LOGINWIN_H__
 
+#include "BaseWin.h"
+#include "Buttom.h"
+#include "InputWin.h"
+#include <vector>
+using std::vector;
+
+class LoginWin : public BaseWin {
+public:
+    enum STATUS{
+        LOGIN,
+        REGISTER
+    };
+private:
+    STATUS stat;
+    Buttom Okey, Cancel;
+    vector<InputWin> IW;
+};
+
 #endif
