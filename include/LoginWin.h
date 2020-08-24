9458a62dcd3086359a5e4f33ee2ec65a7a468093
wanzzhehe <wanzzmicro@outlook.com>
2020-08-24 18:52:27 +0800

    2020-8-24

--- include/LoginWin.h
+++ include/LoginWin.h
@@ -5,7 +5,10 @@
 #include "Buttom.h"
 #include "InputWin.h"
 #include <vector>
+#include <curses.h>
+#include <string>
 using std::vector;
+using std::string;
 
 class LoginWin : public BaseWin {
 public:
@@ -15,8 +18,14 @@ public:
     };
 private:
     STATUS stat;
-    Buttom Okey, Cancel;
+    Buttom B_Okey, B_Cancel;
     vector<InputWin> IW;
+    vector<BaseWin *> list;
+    string topmesg;
+public:
+    LoginWin();
+    void Init(int starty, int startx, int height, int width, short colorp, STATUS stat, string topmesg);
+    chtype Read();
 };
 
 #endif
--- src/LoginWin.cpp
+++ src/LoginWin.cpp
@@ -1,1 +1,25 @@
 #include "LoginWin.h"
+
+LoginWin::LoginWin() { ;}
+
+void LoginWin::Init(int y, int x, int H, int W, short p, STATUS s, string tm) {
+    BaseWin::Init(y, x, H, W, p);
+    stat = s, topmesg = tm;
+    if(topmesg.length() == 0) {
+        switch(stat) {
+            LOGIN: 
+                topmesg = "Login";
+                break;
+            REGISTER:
+                topmesg = "Register";
+                break;
+        }
+    }
+    switch(stat) {
+        LOGIN:
+            
+            break;
+        REGISTER:
+            break;
+    }
+}
